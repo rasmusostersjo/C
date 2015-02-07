b0e6038f799747574ddc26484c077c70dd038be2
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-02-07 21:40:01 +0100

    Added strnCpy

--- string/String.c
+++ string/String.c
@@ -1,14 +1,32 @@
 #include "String.h"
 
-///////////////////////////// strcpy ////////////////////////////////////////
+///////////////////////////// strCpy ////////////////////////////////////////
 
-char *strcpy(char *dest, const char *src)
+char *strCpy(char *dest, const char* src)
 {
     char *destStart = dest;
 
     // Copy src to dest
-    while (*dest++ = *src++)
+    while ( (*dest++ = *src++) )
         ;
+
+    return destStart;
+}
+
+///////////////////////////// strnCpy ///////////////////////////////////////
+
+char *strnCpy(char *dest, const char* src, size_t n)
+{
+    size_t i        = 0;
+    char *destStart = dest;
+    
+    // Copy n characters and stop if src reached the end
+    while (i < n && (*dest++ = *src++) )
+        ++i;
+
+    // Write additional null bytes if n characters were not written
+    while (i++ < n)
+        *dest++ = 0x00;
 
     return destStart;
 }
--- string/String.h
+++ string/String.h
@@ -1,8 +1,11 @@
 #ifndef STRING_H
 #define STRING_H
 
-/** strcpy
- * Copies a nullterminated string pointer to by src to the buffer pointed to by
+// Libraries
+#include <stddef.h>     // size_t
+
+/** strCpy
+ * Copies a null-terminated string pointed to by src to the buffer pointed to by
  * dest. It's the responsibility of the caller to provide a buffer that's large
  * enough.
  *
@@ -10,7 +13,21 @@
  * @param src       Pointer to a string.
  * @return          A pointer to the the destination string.
  */
-char *strcpy(char *dest, const char *src);
+char *strCpy(char *dest, const char* src);
 
+/** strnCpy
+ * Copies a null-terminated string pointed to by src to the buffer pointed to by
+ * dest. At most n characters are copied, and if there's no terminating null
+ * byte among that interval dest will not be null-terminated. If the string
+ * pointed to by src contains less characters than n strnCpy writes additional m
+ * null bytes to ensure n characters were written. Note that it's the
+ * responsibility of the caller to procive a buffer that's large enough.
+ *
+ * @param dest      Pointer to a buffer.
+ * @param src       Pointer to a string.
+ * @param n         Amount of characters to copy.
+ * @return          A pointer to the the destination string.
+ */
+char *strnCpy(char *dest, const char* src, size_t n);
 
 #endif // STRING_H
