383930b725c1878015497fbaa2703cec3522c428
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-03-02 11:14:51 +0100

    Simplified memCpy code

--- string/memCpy.c
+++ string/memCpy.c
@@ -2,31 +2,23 @@
 
 void* memCpy(void* dest, const void* src, size_t n)
 {
-    char*         byteDest = (char*)dest;
-    const char*   byteSrc  = (const char*)src;
     size_t*       wordDest = (size_t*)dest;
     const size_t* wordSrc  = (size_t*)src;
     void*         ret      = dest;
     size_t        wordSize = sizeof(size_t);
+    char*         byteDest;
+    const char*   byteSrc;
 
-    // src or dest does not have word aligned addresses -- cpy byte by byte
-    if ( (size_t)dest % wordSize || (size_t)src % wordSize )
-        while (n--)
-            *byteDest++ = *byteSrc++;
-
-    // Word aligned addresses -- cpy word by word
-    else {
-
-        // Copy words
+    // src, dest word aligned -- cpy word by word  
+    if ( !((size_t)dest % wordSize) && !((size_t)src % wordSize) )
         for (; n >= wordSize; n -= wordSize)
             *wordDest++ = *wordSrc++;
 
-        // Copy remaining bytes
-        byteDest = (char*)wordDest;
-        byteSrc  = (const char*)wordSrc;
-        while (n--)
-            *byteDest++ = *byteSrc++;
-    }
+    // Copy remaining bytes OR dest, src not word aligned
+    byteDest = (char*)wordDest;
+    byteSrc  = (const char*)wordSrc;
+    while (n--)
+        *byteDest++ = *byteSrc++;
 
     return ret;
 }
