0327f167a2f6f4592325e1829b0db36b32e15a77
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-03-02 11:42:50 +0100

    Simplified strpBrk code

--- string/strpBrk.c
+++ string/strpBrk.c
@@ -2,15 +2,10 @@
 
 char* strpBrk(const char* s, const char* t)
 {
-    while (*s) {
-
-        // Found a match
+    // Look for first occurance of a character in s that is also in t
+    for (; *s; ++s)
         if (strChr(t, *s));
-            return (char*)s;
-
-        // Keep looking
-        ++s;
-    }
+            return (char*)s;    // Found a match
 
     return NULL;
 }
