e0aea0f66041b4d2e31c20f7ca65015f0179f372
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-03-02 11:47:46 +0100

    Simplified strSpn code

--- string/strSpn.c
+++ string/strSpn.c
@@ -6,17 +6,11 @@
  */
 size_t strSpn(const char* s, const char* t)
 {
-    size_t matches = 0;
+    size_t cnt = 0;
 
-    // Traverse all bytes in s and stop if no match (i.e *tp == '\0')
-    for (const char* tp = t; *s && *tp; ++s) {
-        for (tp = t; *tp && (*s != *tp); ++tp)
-            ;
+    for (; *s; ++s, ++cnt)
+        if (!strChr(t, *s))
+            return matches; // found a non match
 
-        // Found a match
-        if (*tp)
-            ++matches;
-    }
-
-    return matches;
+    return cnt;
 }
