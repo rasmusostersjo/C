0ddb944a9eb5ab9750dbc14f9ae3536e5da7fd56
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-03-02 11:03:53 +0100

    Simplified memSet code

--- string/memSet.c
+++ string/memSet.c
@@ -3,19 +3,14 @@
 void* memSet(void* dest, int c, size_t n)
 {
     void*   ret      = dest;
-    char*   byteDest = (char*)dest;
     size_t* wordDest = (size_t*)dest;
     size_t  wordSize = sizeof(size_t);
-
-    // dest address is not word aligned -- cpy byte by byte
-    if ( (size_t)dest % wordSize )
-        while (n--)
-            *byteDest++ = (char)c;
+    char*   byteDest;
 
     // dest address is word aligned -- cpy word by word
-    else {
+    if ( (size_t)dest % wordSize ) {
         size_t word   = c;
-        size_t offset = 0x4;  // *2 in first shift --> initially byte offset
+        size_t offset = 0x4;
         
         // Set up a word -- every byte consisting of the byte c
         while ( (wordSize >>= 1) )
@@ -24,12 +19,12 @@ void* memSet(void* dest, int c, size_t n)
         // Copy words
         for (wordSize = sizeof(size_t); n >= wordSize; n -= wordSize)
             *wordDest++ = word;
-
-        // Copy remaining bytes
-        byteDest = (char*)wordDest;
-        while (n--)
-            *byteDest++ = (char)c;
     }
+
+    // Copy remaining bytes OR dest was not word aligned --> cpy byte by byte
+    byteDest = (char*)wordDest;
+    while (n--)
+        *byteDest++ = (char)c;
 
     return ret;
 }
