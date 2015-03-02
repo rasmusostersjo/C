04df7e7f01968254790a847f160574aa056e5625
Rasmus Östersjö <rasmus.ostersjo@gmail.com>
2015-03-02 12:23:18 +0100

    Added strCpy tests

---
+++ string/Test.h
@@ -0,0 +1,14 @@
+#ifndef TEST_H
+#define TEST_H
+
+/** test_strCpy
+ * Performed tests:
+ *  -- Copy empty string
+ *  -- Copy normal string
+ *  -- Copy sneaky string
+ *  -- Copy random string
+ *  -- Return value
+ */
+int test_strCpy(void);
+
+#endif // TEST_H
